caaf662fc65b6900575a657c5562bad6856fa155
TheOnlineCat <116917320+TheOnlineCat@users.noreply.github.com>
2023-10-10 11:36:56 +0800

    updates

--- src/3x3.ts
+++ src/3x3.ts
@@ -26,19 +26,19 @@ export function sleep(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
-export namespace Slot {
+export namespace Slots {
     const Texture = PIXI.Texture;
     const path = Config.images;
 
     const Resources = [
         `${path}/joker.jpg`,
+        `${path}/clown.jpeg`
     ]
 
     enum Face
     {
         JOKER = 0,
         REDCLOWN = 1,
-
     }
     class Tile extends PIXI.Sprite{
         static Size = 80;
@@ -49,16 +49,27 @@ export namespace Slot {
             this.height = Tile.Size;
             this.width = Tile.Size;
         }
+
+        static RandomFace(){
+            return(Math.floor(Math.random() * Object.keys(Face).length / 2))
+        }
     }
     class Column extends PIXI.Sprite{
-        stripLength: number = 5;
-        speed: number = 20;
+        private stripLength: number = 5;
+        private speed: number = 20;
 
-        tileArray : PIXI.Sprite[];
-        currentTile = 0;
+        private currentSpeed: number = 20;
+        private elapsedTime: number = 0;
+        private tileArray : PIXI.Sprite[];
+        private currentTile: number = 0;
+        private targetTiles: number = 50;
+        private resultTiles: Tile[] = [];
 
-        constructor(length: number = 0, spinSpeed: number = 20) {
+        public ctx: Slot;
+
+        constructor(slot: Slot, length: number = 0, spinSpeed: number = 20) {
             super();
+            this.ctx = slot;
             this.stripLength = length + 2;
             this.speed = spinSpeed;
 
@@ -69,21 +80,99 @@ export namespace Slot {
                 this.tileArray[i] = tile;
                 this.addChild(tile);
             }
+            
             this.anchor.set(0.5);
             this.x = app.screen.width/2;
             this.y = app.screen.height/2;
         }
 
-        Spin(delta: number) {
-            this.y += delta * this.speed
+        private GenerateTile(face: Face) {
+            let tile =  new Tile(face);
+            tile.y = this.tileArray[this.tileArray.length - 1].position.y - Tile.Size;
+            return(tile)
+        }
+
+        private AddTile(tile: Tile) {
+            this.addChild(tile)
+            this.tileArray.push(tile)
+            this.tileArray[0].destroy();
+            this.tileArray.shift()
+        }
+
+        Spin(resultTiles: Face[]) {
+
+            resultTiles.forEach(face => {
+                this.resultTiles.push(this.GenerateTile(face))
+            });
+
+            this.currentSpeed = this.speed;
+            this.elapsedTime = 0;
+            this.currentTile = 0;
+            app.ticker.remove(this.UpdateSpin);
+            app.ticker.add((delta) => this.UpdateSpin(delta));
+            this.StopSpin(200);
+        }
+
+        UpdateSpin(delta: number) {
+            this.y += delta * this.currentSpeed
+
+            let relativePos = (this.position.y - (app.screen.height/2))
+            let tileElapsed = Math.floor((relativePos / Tile.Size));
+
+            if (tileElapsed < (this.targetTiles - this.stripLength + 1)) {
+                for(let i = 0; i < tileElapsed - this.currentTile; i++) {
+                    let tile =  this.GenerateTile(Tile.RandomFace())
+                    this.AddTile(tile)
+                }
+            }
+            else if (tileElapsed < this.targetTiles) {
+                for(let i = 0; i < tileElapsed - this.currentTile; i++) {
+                    let tile = this.resultTiles.shift();
+                    if (tile instanceof Tile) {
+                        this.addChild(tile)
+                        this.tileArray.push(tile)
+                        console.log("g" + this)
+                    }
+                    else {
+                        let tile =  this.GenerateTile(Tile.RandomFace())
+                        this.AddTile(tile)
+                    }
+                    
+                }
+                // let leftover = this.targetTiles - tileElapsed -this.resultTiles.length;
+                // for(let i = 0; i < leftover; i++){
+                //     let tile =  this.GenerateTile(Tile.RandomFace())
+                //     this.AddTile(tile)
+                // }
+            }
+
+            this.currentTile = tileElapsed;
+        }
+
+        async StopSpin(freq: number){
+            while(this.currentTile < this.targetTiles) {
+                this.currentSpeed = this.speed * (1.1 - (this.currentTile / this.targetTiles));  
+                
+                await sleep(freq);
+            }
+            this.currentSpeed = 0;
+
+            for(let i = 0; i < this.resultTiles.length; i++) {
+                this.tileArray[0].destroy();
+                this.tileArray.shift()
+            }
+        }
+
+
+        NatualUpdateSpin(delta: number) {
+            this.y += delta * this.currentSpeed
             
             let relativePos = (this.position.y - (app.screen.height/2))
             let tileElapsed = Math.floor((relativePos / Tile.Size));
 
             if (tileElapsed > this.currentTile) {
                 for(let i = 0; i < tileElapsed - this.currentTile; i++) {
-                    let tile =  new Tile(Face.JOKER);
-                    tile.y = this.tileArray[this.stripLength-1].position.y - Tile.Size;
+                    let tile =  this.GenerateTile(Tile.RandomFace())
                     this.addChild(tile)
                     this.tileArray.push(tile)
                     this.tileArray[0].destroy();
@@ -91,58 +180,76 @@ export namespace Slot {
                 }
                 this.currentTile = tileElapsed;
             }
+        }    
+
+        async NaturalStopSpin(freq: number){
+            let totalTime = this.ctx.SpinTime * 1000;
+            while(this.elapsedTime < totalTime) {
+                this.elapsedTime += freq;
+                this.currentSpeed = this.speed *  (1 - (this.elapsedTime / totalTime));  
+                
+                await sleep(freq);
+            }
+
+            this.currentSpeed = 0;
         }
+
+        
     }
 
-    export class Slots extends PIXI.Sprite{
-        columns: number = 7;
-        rows: number = 5
-        gap: number = 5;
-        spinSpeed: number = 10
-        cascadeDelay: number = 150;
+    export class Slot extends PIXI.Sprite{
+        private columns: number = 3;
+        private rows: number = 3
+        private gap: number = 5;
+        private cascadeDelay: number = 150;
 
-        elapsedTime = 0;
+        public spinTime: number = 5;
+        public spinSpeed: number = 50;
 
-        slots: Column[] = []
+        private slots: Column[] = [];
 
         constructor() {
-            super()
+            super();
+            this.eventMode = 'static';
+            this.GenerateColumn();
+
+            this.on('click', () => {
+                slot.Spin();
+            })
+        }
+
+        public get SpinTime() {
+            return(this.spinTime)
+        }
+
+        private GenerateColumn() {
             let positionX = (Tile.Size + this.gap);
             let startX = app.screen.width/2 - (positionX * this.columns/2)
-
             for(let i = 0; i < this.columns; i++) {
-                const column = new Column(this.rows, this.spinSpeed);
+                const column = new Column(this, this.rows, this.spinSpeed);
                 column.position.x = startX + (positionX * i);
                 this.addChild(column);
                 this.slots.push(column);
             }
-            
         }
 
 
         async Spin() {
             app.ticker.start();
+
+            let faces: Face[] = [Face.JOKER, Face.JOKER, Face.JOKER, Face.JOKER, Face.JOKER]
+
             for(let i = 0; i < this.columns; i++) {
+                this.slots[i].Spin(faces);
                 await sleep(this.cascadeDelay);
-                app.ticker.add((delta) => this.slots[i].Spin(delta));
             }
         }
-
     }
 }
 
 document.getElementById('pixi-container')?.appendChild(app.view);
 
-const slot = new Slot.Slots();
-slot.eventMode = 'static';
+const slot = new Slots.Slot();
 app.stage.addChild(slot);
-app.renderer.render(app.stage);
 
-console.log("Started Successfully" + Math.random())
-
-slot.on('click', () => {
-    // Your click event handler logic here
-    console.log('Sprite clicked!');
-    slot.Spin();
-})
 
